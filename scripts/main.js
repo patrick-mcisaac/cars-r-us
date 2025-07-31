546e210ddac1977d95a697b0942d5caccb5318d4
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-07-31 11:57:55 -0500

    set up basic html structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,35 @@
 
+const header = document.getElementById('header')
+const container = document.getElementById('container')
 
 const render = () => {
-    
+    header.innerHTML = `<h1>Cars 'R Us: Personal </h1>`
+
+    container.innerHTML = `
+    <section>
+
+        <article>
+            <h2>Paints</h2>
+        </article>
+
+        <article>
+            <h2>Interior</h2>
+        </article>
+
+        <article>
+            <h2>Wheels</h2>
+        </article>
+
+        <article>
+            <h2>Technologies</h2>
+        </article>
+
+    </section>
+
+    <section>
+        <h2>Custom Car Orders</h2>
+        
+    </section>
+
+    `
 }
